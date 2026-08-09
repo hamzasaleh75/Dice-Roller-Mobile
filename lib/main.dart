46e34a75380f4247927d1b80eb3c7b19db76e612
hamza <hamzasaleh005@gmail.com>
2026-08-09 03:30:16 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/graient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer.purple(),
+        ),
+        ),
+        );
+}
